432c97657b84a1a015ca2589ebee0daacbabadab
Kustov Ilya <kusgent@yandex.ru>
2023-11-22 11:35:43 +0300

    Add solutions for lesson 4

---
+++ 04/01.py
@@ -0,0 +1,13 @@
+number = 5
+digit = 4.5544
+word = "Результат:"
+boolean = True
+
+print(word, digit)
+print(word, boolean)
+print(word + str(boolean))
+
+del number
+
+number = 8
+print("Результат:", number)
---
+++ 04/02.py
@@ -0,0 +1,12 @@
+num1 = int(input("Введите первое число: "))
+num2 = int(input("Введите второе число: "))
+
+num2 += 6
+
+print("Результат:", num1 + num2)
+print("Результат:", num1 - num2)
+print("Результат:", num1 * num2)
+print("Результат:", num1 / num2)
+
+str1 = "раз"
+print(str1 * 5)
