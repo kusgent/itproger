012ae6d2f1b2258982bea201e496f6b89209cdb8
Kustov Ilya <kusgent@yandex.ru>
2023-11-28 23:23:04 +0300

    Add 01.py

---
+++ 08/01.py
@@ -0,0 +1,15 @@
+word = "iTproger"
+words = "One, two, three"
+words_list = words.split(", ")
+
+print(word[3])
+print(len(word))
+print(word.count("r"))
+print(word.count("q"))
+print(word.upper())
+print(word.isupper())
+print(word.lower())
+print(word.islower())
+print(word.capitalize())
+print(word.find("pr"))
+print(words_list[1])
