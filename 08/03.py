5e70bb952c594715e90e1a7c8a30b3d66678ba07
Kustov Ilya <kusgent@yandex.ru>
2023-11-29 16:34:42 +0300

    Add 03.py

---
+++ 08/03.py
@@ -0,0 +1,13 @@
+sport = "basketball"
+
+print(sport[3:6])
+print(sport[:])
+print(sport[::2])
+print(sport[::-1])
+
+data = [3, 7, "Test", 2.9, -5, False]
+
+print(data[1:-2])
+print(type(data[3]), data[3])
+print(data[1::2])
+print(data[::-1])
