a498263fdbf913133be8394b530824c8d5c1ef2c
Kustov Ilya <kusgent@yandex.ru>
2024-03-12 21:32:04 +0300

    Add solution for lesson 20

---
+++ 20/01.py
@@ -0,0 +1,18 @@
+import webbrowser
+
+
+def validator(func):
+    def wrapper(url):
+        if "." in url:
+            func(url)
+        else:
+            print("Неверный URL")
+    return wrapper
+
+
+@validator
+def open_url(url):
+    webbrowser.open(url)
+
+
+open_url("https://yandex.ru")
